ab9c6532c8d9464e80e1d79a95496f99fe3a8ddf
Liltar <liltar94@gmail.com>
2021-02-26 19:46:08 +0200

    init commit

---
+++ lesson_1/task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter age:");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter adress:");
+            string adress = Console.ReadLine();
+
+            Console.WriteLine("Enter phone:");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Enter height:");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(adress);
+            Console.WriteLine(phone);
+            Console.WriteLine(height);
+
+        }
+    }
+}
