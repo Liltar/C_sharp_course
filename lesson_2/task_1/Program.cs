50c3371fabf0dda856a063d452e0fc829dcc888f
Liltar <liltar94@gmail.com>
2021-03-02 16:59:40 +0200

    init commit

---
+++ lesson_2/task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            name = name.Trim();
+
+            Console.WriteLine("Enter age:");
+            string age = Console.ReadLine();
+            int age_int = int.Parse(age);
+            if (age_int < 18)
+                {
+                Console.WriteLine("You should be minimum 18 years old to gamble");
+            }
+            else
+            {
+                Console.WriteLine("Place your bet:");
+                string bet = Console.ReadLine();
+                int bet_int = int.Parse(bet);
+
+
+            }
+        }
+    }
+}
