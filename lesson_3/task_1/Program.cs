46c814b209e7d832370626486a10b3698e89cc1c
Liltar <liltar94@gmail.com>
2021-03-29 00:44:38 +0300

    init commit

---
+++ lesson_3/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//8.Умножение числа а = 12345679 на числа 9, 18, 27, ... 81 дает интересные результаты. 
+//Напишите программу получения этих произведений.
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123456789;
+            int b = 9;           
+            for (int i = 1; i < 10; i++)
+            {
+                int c = b * i;
+                Console.WriteLine(a * c);
+            }
+        }
+    }
+}
